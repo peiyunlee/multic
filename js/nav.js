1b15746cbf6054a7098f6ffec06728f8c043a1a5
PEI-YUN LEE <may1092200258@gmail.com>
2020-01-05 01:01:41 +0800

    hamberger

---
+++ js/nav.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+  let i=0;
+  $('#hamberger1').click(function (){
+    i++;
+    console.log("click")
+    if(i==1){
+        $('#con_navbar1').css("position",'fixed');
+        TweenLite.to("#s_nav1", 0.5, {
+            opacity:"1",display:"grid"
+        })
+        TweenLite.to("#con_navbar2", 0 , {
+            opacity:"0",display:"none"
+        })
+    }
+    else{
+        $('#con_navbar1').css("position",'absolute');
+        TweenLite.to("#s_nav1", 0.5, {
+            opacity:"0",display:"none"
+        })
+        TweenLite.to("#con_navbar2", 0 , {
+            opacity:"1",display:"flex"
+        })
+      i=0
+    }
+  })
+
+  let j=0;
+  $('#hamberger2').click(function (){
+    j++;
+    if(j==1){
+        $('#con_navbar2').css("position",'fixed');
+        TweenLite.to("#s_nav2", 0.5, {
+            opacity:"1",display:"grid"
+        })
+        TweenLite.to("#con_navbar1", 0 , {
+            opacity:"0",display:"none"
+        })
+        TweenLite.to(".con_htop", 0 , {
+            opacity:"0",display:"none"
+        })
+    }
+    else{
+        $('#con_navbar2').css("position",'absolute');
+        TweenLite.to("#s_nav2", 0.5, {
+            opacity:"0",display:"none"
+        })
+        TweenLite.to("#con_navbar1", 0 , {
+            opacity:"1",display:"flex"
+        })
+        TweenLite.to(".con_htop", 0 , {
+            opacity:"1",display:"flex"
+        })
+      j=0
+    }
+  })
+})
