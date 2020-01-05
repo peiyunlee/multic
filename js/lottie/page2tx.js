ea2d755f1c966bcbf1d04ce7d4cd9f0bc5b4e997
Albada <NamelessOAO@gmail.com>
2020-01-05 21:27:32 +0800

    page2lottie

---
+++ js/lottie/page2tx.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    let openingAnimWindow = document.querySelector('#lottie4');
+    let openingAnimData = {
+        container: openingAnimWindow,
+        animType: 'svg',
+        loop: true,
+        prerender: true,
+        autoplay: true,
+        path: '../json/page2tx.json'
+    };
+    // set bodymovin
+    let openingAnim = bodymovin.loadAnimation(openingAnimData);
+});
