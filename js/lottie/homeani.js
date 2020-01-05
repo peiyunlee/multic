6738287bfb3208d9450807b553624143f93c6268
Albada <NamelessOAO@gmail.com>
2020-01-05 13:36:56 +0800

    navlogo

--- js/lottie/homeani.js
+++ js/lottie/homeani.js
@@ -5,9 +5,11 @@ $(document).ready(function() {
         animType: 'svg',
         loop: false,
         prerender: true,
-        autoplay: true,
+        autoplay: false,
         path: '../json/logani.json'
     };
     // set bodymovin
     let openingAnim = bodymovin.loadAnimation(openingAnimData);
-})
+    var myVar;
+    myVar = setTimeout(function() { openingAnim.play(); }, 750);
+});
