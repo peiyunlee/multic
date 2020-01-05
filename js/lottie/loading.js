c8aa6152c14d3e7225275bf5842364f676065c66
Albada <NamelessOAO@gmail.com>
2020-01-06 00:05:54 +0800

    loading

---
+++ js/lottie/loading.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    let openingAnimWindow = document.querySelector('#loadani');
+    let openingAnimData = {
+        container: openingAnimWindow,
+        animType: 'svg',
+        loop: true,
+        prerender: true,
+        autoplay: false,
+        path: '../json/cirlogo.json'
+    };
+    // set bodymovin
+    let openingAnim = bodymovin.loadAnimation(openingAnimData);
+    // openingAnim.addEventListener('DOMLoaded', loopanimate);
+
+    // function loopanimate() {
+    //     console.log('3')
+    //     openingAnim.playSegments([0, 16], true);
+    // }
+    window.onload = function() {
+        console.log(2)
+        openingAnim.playSegments([0, 41], true);
+        $('#load').css("display", "none");
+        $('#load').css("transition", "1s");
+    };
+});
